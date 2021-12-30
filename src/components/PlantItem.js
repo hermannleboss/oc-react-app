de869dfcdbc7dd5d0d0b74cc0a6ebd015150536c
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-30 20:08:37 +0000

    Event on PlantItem

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,17 +1,20 @@
 import CareScale from './CareScale'
 import '../../../oc-react-app/src/styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
-	return (
-		<li key={id} className='lmj-plant-item'>
-			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
-			{name}
-			<div>
-				<CareScale careType='water' scaleValue={water} />
-				<CareScale careType='light' scaleValue={light} />
-			</div>
-		</li>
-	)
+function PlantItem({id, cover, name, water, light}) {
+    return (
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+            <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`}/>
+            {name}
+            <div>
+                <CareScale careType='water' scaleValue={water}/>
+                <CareScale careType='light' scaleValue={light}/>
+            </div>
+        </li>
+    )
 }
 
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
 export default PlantItem
