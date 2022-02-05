70fd0d5dbc20d614a4a4063a6ac85ec53b0bed81
EHO K. Hermann <hermannleboss@gmail.com>
2022-02-05 10:46:13 +0000

    P3C2 partage des states entre différents composants

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,25 +1,45 @@
 import '../styles/Cart.css';
 import { useState } from 'react';
+
 function Cart({ cart, updateCart }) {
-  const monsteraPrice = 8
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const total = cart.reduce(
+    (acc, plantType) => acc + plantType.amount * plantType.price,
+    0,
+  );
 
   return isOpen ? (
-    <div className='lmj-cart'>
-      <button onClick={() => setIsOpen(false)}>Fermer</button>
-      <h2>Panier</h2>
-      <div>
-        Monstera : {monsteraPrice}€
-        <button onClick={() => updateCart(cart + 1)}>
-          Ajouter
-        </button>
-      </div>
-      <h3>Total : {monsteraPrice * cart}€</h3>
-      <button onClick={() => updateCart(0)}>Vider le panier</button>
+    <div className="lmj-cart">
+      <button className="lmj-cart-toggle-button" onClick={() => setIsOpen(false)}>
+        Fermer
+      </button>
+      {cart.length > 0 ? (
+        <div>
+          <h2>Panier</h2>
+          <ul>
+            {cart.map(({ name, price, amount }, index) => (
+              <div key={`${name}-${index}`}>
+                {name} {price}€ x {amount}
+              </div>
+            ))}
+          </ul>
+          <h3>Total :{total}€</h3>
+          <button onClick={() => updateCart([])}>Vider le panier</button>
+        </div>
+      ) : (
+        <div>Votre panier est vide</div>
+      )}
     </div>
   ) : (
-    <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
-  )
+    <div className="lmj-cart-closed">
+      <button
+        className="lmj-cart-toggle-button"
+        onClick={() => setIsOpen(true)}
+      >
+        Ouvrir le Panier
+      </button>
+    </div>
+  );
 }
 
 export default Cart;
